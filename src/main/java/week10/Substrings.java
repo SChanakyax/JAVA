0a291434f1e1ce4ae2b23b4a9676fc46f8b9629d
SChanakyax <chanakyachana@gmail.com>
2023-10-26 17:43:02 +0530

    String

--- src/main/java/week10/Substrings.java
+++ src/main/java/week10/Substrings.java
@@ -4,5 +4,12 @@ public class Substrings {
 
     public static void main(String[] args) {
 
+        String itNo = "IT19122145";
+
+        String it = itNo.substring(0,2);
+        String year = itNo.substring(2,4);
+        String fac = itNo.substring(4,8);
+        String index = itNo.substring(8,10);
+        System.out.println(it + " " + year + " " + fac + " " + index);
     }
 }
